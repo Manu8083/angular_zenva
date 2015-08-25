665eff09c9925abb7f131e9663509e275acf9d45
manuel romero <manu8083el@gmail.com>
2015-08-24 23:36:31 -0500

    termina conceptos basicos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module("expensesApp" , []);
 
 app.controller('ExpensesViewController', [ '$scope', function ($scope) {
- $scope.name = "Manuel";
+ $scope.name = "Manuel"; // se define variable name
 $scope.expense = {
 	description: 'food' ,
 	amount: 10
@@ -10,4 +10,14 @@ $scope.phrase = 'Es una frase cualquiera';
 $scope.increaseAmount = function () {
 	$scope.expense.amount++;
 };
+$scope.items = [
+	{
+		name:'pizza' ,
+		ingredients: ['queso' , 'pan' , 'bocadillito' , 'piña']
+	},
+	{
+		name:'tortilla',
+		ingredients: ['pan', 'sal' , 'pepper' , 'garlic']
+	}
+];
 }]);
