6062cf96bab53910aabdb72931a7ef8eef1b164e
manuel romero <manu8083el@gmail.com>
2015-08-30 20:57:21 -0500

    prueba nicolas

--- js/app.js
+++ js/app.js
@@ -1,11 +1,36 @@
-var app = angular.module('expensesApp', []);
+var app = angular.module('expensesApp', ['ngRoute']);
+
+
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'index.html',
+			controller: 'ExpensesViewController'
+	})
+		.when('/expenses' , {
+			templateUrl: 'index.html' ,
+			controller: 'ExpensesViewController'
+	})
+		.when('/expenses/new',{
+			templateUrl:'view/from.html',
+			controller: 'ExpenseViewController'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+});
 
 app.controller('HomeViewController', [ '$scope', function ($scope) {
  $scope.apptitle = 'aplicacion Manutromero';
 }]);
 app.controller('ExpensesViewController',['$scope' , function($scope) {
 	$scope.expenses =[
-		{description: 'food' , amount:10 , date:'2015-03-11'},
-		{description: 'comida' , amount:30 , date:'2015-03-12'},
+		{description: 'fooOd' , amount:10 , date:'2015-03-11'},
+		{description: 'Comida' , amount:30 , date:'2015-03-12'},
+		{description: 'CoMiDa' , amount:30 , date:'2015-03-12'},
 	];
+}]);
+
+app.controller('ExpenseViewController',['$scope' , function($scope){
+	$scope.sometext = 'The Word is round';
 }]);
